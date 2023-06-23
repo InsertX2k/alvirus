d3bc9e87ae55af5cc5a75d55b0c85506eb992a2c
Ziad <62176660+InsertX2k@users.noreply.github.com>
2023-06-23 16:37:38 +0300

    edit

--- python.files/uac-disable.py
+++ python.files/uac-disable.py
@@ -1,3 +1,2 @@
-import os
-import WINTCMD
-WINTCMD.term('reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d 0 /f')
+from os import system
+system('reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableLUA" /t REG_DWORD /d 0 /f')
